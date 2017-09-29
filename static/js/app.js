f0db00bd98d68c2e92b0c03bc6da6fd9d93a22cd
Alex Nerru <karpinaleksandr1999@gmail.com>
2017-09-29 23:32:00 +0300

    Add cards instead of list of links

--- static/js/app.js
+++ static/js/app.js
@@ -5,6 +5,7 @@ listOFLinks = [];
 Dropzone.options.myAwesomeDropzone = {
         maxFilesize: 20, // Size in MB
         dictDefaultMessage: "Перенесите файлы сюда",
+        previewsContainer:
         removedfile: function (file) {
             var fileRef;
             return (fileRef = file.previewElement) != null ?
@@ -12,8 +13,10 @@ Dropzone.options.myAwesomeDropzone = {
         },
 
         success: function (file, response) {
-            document.getElementById("links").innerHTML = "Ссылки";
-            addToListAndPrint(response);
+            createZone();
+            createCardLinkArea();
+
+            createNewCard(response);
         },
 
         error: function (file, response) {
@@ -22,15 +25,39 @@ Dropzone.options.myAwesomeDropzone = {
 
     };
 
-function addToListAndPrint(response) {
+/*function addToListAndPrint(response) {
     var newLI = document.createElement("li");
     displaySpellList = document.getElementById("listOfLinks");
     newContent = document.createTextNode(response);
     newLI.appendChild(newContent);
     displaySpellList.appendChild(newLI);
 
+}*/
+function createZone() {
+    console.log(1);
+    var div =  document.createElement('div');
+    div.className = 'card';
+    div.id = 'cardZone';
+    cardZone = document.getElementById("cards");
+    cardZone.appendChild(div);
+    
+}
+function createCardLinkArea() {
+   
+     var div = document.createElement('div');
+     div.className = 'list-group list-group-flush';
+     div.id = "cardList";
+     cardLinks = document.getElementById("cardZone");
+     cardLinks.appendChild(div);
+
 }
  function createNewCard(response) {
-     
+     //var a1 = "<div class=\"card\"> <div class=\"card-body\"> <div class=\"card-title\"> Карточка  </div> </div></div>";
+     //document.getElementById("cardList").innerHTML = a1;
+     var div = document.createElement('div');
+     div.className = 'list-group-item';
+     div.innerHTML = response;
+     cardList = document.getElementById("cardList");
+     cardList.appendChild(div);
  }
 
