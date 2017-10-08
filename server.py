7a9fd6f25b185bf75367b57798b5e34371c4c231
Alex Nerru <karpinaleksandr1999@gmail.com>
2017-10-08 20:54:15 +0300

    Minor bugs fixed

--- server.py
+++ server.py
@@ -14,12 +14,15 @@ class MainHandler (tornado.web.RequestHandler):
 			file_name = file['filename']
 			extension = os.path.splitext(file_name)[1]
 			finalName = str(uuid.uuid4()) + str(extension)
-			with open(os.path.join(os.path.dirname(os.path.realpath(__file__)),'images') + finalName, 'wb') as f:
+			file_path_small = os.path.join(os.path.dirname(os.path.realpath(__file__)),
+							     'images')
+			file_path = os.path.join (file_path_small,finalName)
+			with open(file_path, 'wb') as f:
 				f.write(file['body'])
 			direct_url = 'https://storage.geekclass.ru/images/' + finalName
 			markdown_url = '![](https://storage.geekclass.ru/images/'+ finalName + ')'
 			html_url =  '<img src="https://storage.geekclass.ru/images/"'+finalName+'/>'
-			data ={"direct_url":direct_url, "markdown": markdown_url, "html" : html_url}
+			data ={"direct":direct_url, "markdown": markdown_url, "html" : html_url}
 			self.write(json.dumps(data))
 
 
@@ -29,5 +32,5 @@ settings = [
     (r'/images/(.*)', tornado.web.StaticFileHandler, {'path': os.path.join(os.path.dirname(os.path.realpath(__file__)),'images')}),
 ]
 app = tornado.web.Application(settings)
-app.listen(8883)
+app.listen(8888)
 tornado.ioloop.IOLoop.current().start()
--- static/js/app.js
+++ static/js/app.js
@@ -1,121 +1,108 @@
-
-
 listOFLinks = [];
 
 Dropzone.options.myAwesomeDropzone = {
-        maxFilesize: 20, // Size in MB
-        dictDefaultMessage: "Перенесите файлы сюда или кликните для загрузки",
-        previewsContainer: false,
-        removedfile: function (file) {
-            var fileRef;
-            return (fileRef = file.previewElement) != null ?
-                fileRef.parentNode.removeChild(file.previewElement) : void 0;
-        },
+    maxFilesize: 20, // Size in MB
+    dictDefaultMessage: "Перенесите файлы сюда или кликните для загрузки",
+    previewsContainer: false,
+    removedfile: function (file) {
+        var fileRef;
+        return (fileRef = file.previewElement) != null ?
+            fileRef.parentNode.removeChild(file.previewElement) : void 0;
+    },
 
-        success: function (file, response) {
-            createZone();
-            createCardLinkArea();
+    success: function (file, response) {
+        createZone();
+        createCardLinkArea();
+        createNewCard(file, response);
+    },
 
-            createNewCard(response, file);
-        },
+    error: function (file, response) {
+        alert(response);
+    }
 
-        error: function (file, response) {
-            alert(response);
-        }
-
-    };
+};
 
 function createZone() {
-    console.log(1);
-    var div =  document.createElement('div');
+    var div = document.createElement('div');
     div.className = 'card';
     div.id = 'cardZone';
     cardZone = document.getElementById("cards");
     cardZone.appendChild(div);
-    
+
 }
+
 function createCardLinkArea() {
-   
-     var div = document.createElement('div');
-     div.className = 'list-group list-group-flush';
-     div.id = "cardList";
-     cardLinks = document.getElementById("cardZone");
-     cardLinks.appendChild(div);
+
+    var div = document.createElement('div');
+    div.className = 'list-group list-group-flush';
+    div.id = "cardList";
+    cardLinks = document.getElementById("cardZone");
+    cardLinks.appendChild(div);
 
 }
- function createNewCard(response, file) {
-     var data = JSON.parse(response);
-     console.log(data);
-     var img = document.createElement('img');
-     img.className = 'card-img-left';
-     img.id = 'img';
-     img.height = 180;
-     img.width = 240;
-     img.align = 'left';
-     img.src = file.dataURL;
 
-     var directLink = document.createElement('input');
-     directLink.value = data.direct_url;
-     directLink.id = 'directLink';
-     directLink.size = 85;
+function createLinkBlock(label, link, id, className) {
+    var div = document.createElement('div');
+    div.className = 'block';
 
-     var markdownLink = document.createElement('input');
-     markdownLink.value = data.markdown;
-     markdownLink.id = 'markdownLink';
-     markdownLink.size = 85;
+    var input = document.createElement('input');
+    input.className = 'link-input form-control';
+    input.value = link;
+    input.id = id;
 
-     var htmlLink= document.createElement('input');
-     htmlLink.value = data.html;
-     htmlLink.id = 'htmlLink';
-     htmlLink.size = 85;
+    var linkLabel = document.createElement("label");
+    linkLabel.for = 'id';
+    linkLabel.innerHTML = label;
+    linkLabel.className = className;
 
-     var headerDirectUrl = document.createElement("H6");
-     headerDirectUrl.align='center';
-     var textNodeDirectLink = document.createTextNode("Прямая ссылка:");
-     headerDirectUrl.appendChild(textNodeDirectLink);
+    div.appendChild(linkLabel);
+    div.appendChild(input);
+    return div;
+}
 
-     var headerMarkDownUrl = document.createElement("H6");
-     headerMarkDownUrl.align='center';
-     headerMarkDownUrl.id = 'headerMarkdownUrl';
-     var textNodeMarkdownLink = document.createTextNode("Markdown:");
-     textNodeMarkdownLink.id = 'textNodeMarkdownLink';
-     headerMarkDownUrl.appendChild(textNodeMarkdownLink);
+function createNewCard(file, response) {
+    var data = JSON.parse(response);
 
-     var headerHTMLUrl = document.createElement("H6");
-     headerHTMLUrl.align='center';
-     headerHTMLUrl.id = 'headerHTMLUrl';
-     var textNodeHTMLLink = document.createTextNode("HTML:");
-     headerHTMLUrl.appendChild(textNodeHTMLLink);
+    var img = document.createElement('img');
+    img.className = 'img-responsive';
+    img.id = 'img';
+    img.src = file.dataURL;
 
-     var insideDiv = document.createElement('div');
-     insideDiv.appendChild(headerDirectUrl);
-     insideDiv.appendChild(directLink);
-     insideDiv.appendChild(headerMarkDownUrl);
-     insideDiv.appendChild(markdownLink);
-     insideDiv.appendChild(headerHTMLUrl);
-     insideDiv.appendChild(htmlLink);
+    var directLink = createLinkBlock('Прямая сслыка:', data.direct, 'directLink', 'link');
+    var markdownLink = createLinkBlock('Markdown:', data.markdown, 'markdownLink', 'link');
+    var htmlLink = createLinkBlock('HTML:', data.html, 'htmlLink', 'link');
+    var insideDiv = document.createElement('div');
+    insideDiv.appendChild(directLink);
+    insideDiv.appendChild(markdownLink);
+    insideDiv.appendChild(htmlLink);
 
-     var div = document.createElement('div');
-     div.className = 'list-group-item';
-     div.id = 'list-group-item';
+    var div = document.createElement('div');
+    div.className = 'list-group-item';
+    div.id = 'list-group-item';
 
-     var row = document.createElement('div');
-     row.className = 'row';
+    var row = document.createElement('div');
+    row.className = 'row';
 
-     var leftColumn = document.createElement('div');
-     leftColumn.className = 'col-md-3';
+    var leftColumn = document.createElement('div');
+    leftColumn.className = 'col-md-3';
 
-     var rightColumn = document.createElement('div');
-     rightColumn.className = 'col-md-9';
-     rightColumn.id = 'rightColumn';
+    var rightColumn = document.createElement('div');
+    rightColumn.className = 'col-md-9';
+    rightColumn.id = 'rightColumn';
+
+    var formGroup = document.createElement('div');
+    formGroup.className = 'form-group';
+    formGroup.appendChild(insideDiv);
+
+    leftColumn.appendChild(img);
+    rightColumn.appendChild(formGroup);
+    row.appendChild(leftColumn);
+    row.appendChild(rightColumn);
+    div.appendChild(row);
+    cardList = document.getElementById("cardList");
+    cardList.appendChild(div);
+}
 
 
-     leftColumn.appendChild(img);
-     rightColumn.appendChild(insideDiv);
-     row.appendChild(leftColumn);
-     row.appendChild(rightColumn);
-     div.appendChild(row);
-     cardList = document.getElementById("cardList");
-     cardList.appendChild(div);
- }
+
 
